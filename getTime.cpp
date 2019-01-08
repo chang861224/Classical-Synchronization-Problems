d71123cb0fb896550b0fb69b7ccb68d21a39f524
Chi-Hung Chang <chihung861224@gmail.com>
2019-01-08 22:55:53 +0800

    2nd Edition

--- getTime.cpp
+++ getTime.cpp
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 using namespace std;
 
-double getTime(double n);
+class getTime{
+	public:
+		getTime(double n);
+		double time;
+};
+
+getTime::getTime(double n){
+	double x = (double)rand() / (RAND_MAX + 1.0);
+	time = (-1) * (x / n);
+}
 
 int main(int argc, char** argv) {
 	double n;
 	
 	cin >> n;
-	cout << getTime(n);
+	
+	cout << getTime(n).time;
 	
 	return 0;
-}
-
-double
-getTime(double n){
-	double x = (double)rand() / (RAND_MAX + 1.0);
-	
-	return (-1) * (x / n);
 }
